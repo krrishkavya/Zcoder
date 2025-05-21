cf17d083dc82a2b93c13b9762962dde9f6df43c2
krrishkavya <krrishkavya193@gmail.com>
2025-05-21 22:29:53 +0530

    Community.jsx edited

--- client/my-app/src/components/Community/Community.jsx
+++ client/my-app/src/components/Community/Community.jsx
@@ -1,8 +1,7 @@
 import RiHeader from "../Header/Header";
 import CommunityChatBox from "./CommunityChatBox";
 //import CommunityChatHeader from "./CommunityChatHeader";
-//footer as it is uthale
-//chat header ar messages banana padega 
+
 function Community(){
    return <>
        <RiHeader/>
